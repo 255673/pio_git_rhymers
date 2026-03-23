e6c97cba3dbaeda10d5857685be4a12ca235df29
Piotr Witczak <255726@edu.p.lodz.pl>
2026-03-23 20:05:52 +0100

    6. Zamiana za liczb za pomocą Extract Constant

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int NUMBERS_12 = 12;
+    public static final int TOTAL_MINUS_1 = -1;
+    public static final int NUMBER_11 = 11;
+    private int[] numbers = new int[NUMBERS_12];
 
-    public int total = -1;
+    public int total = TOTAL_MINUS_1;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL_MINUS_1;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == NUMBER_11;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return TOTAL_MINUS_1;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return TOTAL_MINUS_1;
         return numbers[total--];
     }
 
